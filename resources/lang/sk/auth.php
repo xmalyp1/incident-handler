e62c860c90a711db4cfb7361b85b09652fd8431b
romanstrazanec <strazanec.roman@gmail.com>
2021-03-11 18:24:38 +0100

    sk locale

---
+++ resources/lang/sk/auth.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Zadané údaje sú nesprávne.',
+    'password' => 'Zadané heslo je nesprávne.',
+    'throttle' => 'Príliš mnoho pokusov o prihlásenie. Skúste znova o :seconds sekúnd.',
+
+];
